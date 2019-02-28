a95144bebc8f8fa6d6e5ba737588c51dbb7f4d5e
Hajnal Mate <hajnalmt@gmail.com>
2019-02-28 15:15:26 +0100

    Namespace definition changed a little bit.

--- server_setup.tcl
+++ server_setup.tcl
@@ -3,7 +3,27 @@
 # A TCL, Expect script for environment setup.
 ##############################################################################
 
-namespace eval ::server_setup {
+
+# -----------------------------------------------------------------------------
+# ::eval_server_setup { id args }
+#
+#   This procedure creates an instance of server_setup namespace
+#   (::server_setup_$id)
+#
+#   What it does:
+#   - Creates an instance of server_setup
+#   - Returns namespace name
+# -----------------------------------------------------------------------------
+proc eval_server_setup { id args  } {
+    if { [uplevel 1 namespace current] eq "::"  } {
+        set ns_name "::server_setup_$id"
+    } else {
+        set ns_name "[uplevel 1 namespace current]::server_setup_$id"
+    }
+########################
+# Namespace definition #
+########################
+namespace eval ${ns_name} {
     #Where am I, who ami I, this is for directory independecy
     variable user [exec whoami]
     variable script_path [info script]
@@ -19,11 +39,12 @@ namespace eval ::server_setup {
     #Linux Server Distribution.
     variable server_distro
 }
+
 # -----------------------------------------------------------------------------
 # ::server_setup::usage
 #   Prints the usage information of the script, and then exists
 # -----------------------------------------------------------------------------
-proc ::server_setup::usage {
+proc ${ns_name}::usage {
     variable script_path
     send_user --\
 "This script is used to setup an Ubuntu server. It downloads and upgrades
@@ -40,13 +61,14 @@ Options:
                                 (default is the script user)
                             DOCKER_VER - docker version to install
                                 (default is 18.06)
+    -s, --user-setup    Just setup the user. 
         --setup-home-dirs
                         If you have a home_dirs repository, like:
                         git@github.com:hajnalmt/home-dirs.git
                         Then clone it out.
 Parameters:
     ip|host -- The ip or the host name of the server to configure.
-Example:~
+Example:
     $script_path -e USER=hajnalmt --setup_home_dirs 51.15.239.79
     $script_path my_little_server\n"
     exit
@@ -62,3 +84,5 @@ proc ::server_setup::spawn_ssh {
 }
 
 ::server_setup::usage
+
+
